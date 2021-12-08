a4e827af8caba13c11580ce9264c022e0c41f521
Saran2026 <95347082+Saran2026@users.noreply.github.com>
2021-12-08 20:00:20 +0530

    Update test.js

--- app/controllers/api/v1/test.js
+++ app/controllers/api/v1/test.js
@@ -5,8 +5,9 @@ router.get('/', (req, res) => {
   const result = {};
   const tempvar = 'This is unused varaiable and needs to be reported by sonarscan';
   const tempapi = 'http://localhost:3000';
+  $x = $_GET['arg'];
   result.empId = 'EMP-123';
-  result.empName = 'John';
+  result.empName = 'Saran';
   res.status(200).json({ success: true, data: result });
 });
 
