4838f1a99c27e290001426d6e76f8f14a33e9aed
Saran2026 <95347082+Saran2026@users.noreply.github.com>
2021-12-08 19:47:34 +0530

    Update test.js

--- app/controllers/api/v1/test.js
+++ app/controllers/api/v1/test.js
@@ -3,6 +3,7 @@ const express = require('express'),
 
 router.get('/', (req, res) => {
   const result = {};
+  const tempvar = 'This is unused varaiable and needs to be reported by saonrscan';
   result.empId = 'EMP-123';
   result.empName = 'John';
   res.status(200).json({ success: true, data: result });
